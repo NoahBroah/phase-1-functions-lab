283f585d02e1ac4ef3a54c26aab16afffacf4557
Noah King <noahking123@yahoo.com>
2022-08-08 18:03:52 -0400

    commit tree 675238a30ec426dacd70382782d92eadd12db4f1

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(blocks) {
+    let distance = Math.abs(blocks - 42);
+    return distance;
+}
+
+function distanceFromHqInFeet(blocks) {
+    let distanceFeet = distanceFromHqInBlocks(blocks) * 264;
+    return distanceFeet
+}
+
+function distanceTravelledInFeet(block1, block2) {
+    let distanceTraveled = Math.abs(block1-block2) * 264
+    return distanceTraveled
+}
+
+function calculatesFarePrice(block1,block2) {
+    let destination = distanceTravelledInFeet(block1, block2);
+    if (destination < 400) {
+        return 0;
+    } else if (destination > 400 && destination < 2000) {
+        return (destination - 400) *.02;
+    } else if (destination > 2000 && destination < 2500) {
+        return 25;
+    } else if (destination > 2500) {
+        return 'cannot travel that far';
+    }
+    
+}
